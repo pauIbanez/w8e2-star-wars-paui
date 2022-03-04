3e655daaa7b795a90fa269b8a0e4503e1fccf084
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-04 12:24:11 +0100

    added staship class listing

--- js/main.js
+++ js/main.js
@@ -1,12 +1,40 @@
 const url = "https://swapi.dev/api/starships";
 
 const totalStarships = document.querySelector("#total-ships");
+const starShipsClasses = document.querySelector("#class-list");
 
-const toHtml = (text) => `<p>${text}</p>`;
+const toHtml = (name, count) => `<p>${name}: ${count}</p>`;
 
 (async () => {
   const response = await fetch(url);
   const { count, results } = await response.json();
 
   totalStarships.textContent += ` ${count}`;
+
+  const starshipClasses = [];
+
+  results.forEach((starship) => {
+    const starshipClass = starship.starship_class;
+
+    let classExists = false;
+
+    starshipClasses.forEach((starClass) => {
+      if (starClass.name === starshipClass) {
+        // eslint-disable-next-line no-param-reassign
+        starClass.shipCount += 1;
+        classExists = true;
+      }
+    });
+
+    if (!classExists) {
+      starshipClasses.push({
+        name: starshipClass,
+        shipCount: 1,
+      });
+    }
+  });
+
+  starshipClasses.forEach(({ name, shipCount }) => {
+    starShipsClasses.innerHTML += toHtml(name, shipCount);
+  });
 })();
